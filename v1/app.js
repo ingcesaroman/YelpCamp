8679577f76e7dbd78fcc37d24df660c99c78bfe3
ingcesaroman <ing.cesaroman@gmail.com>
2020-04-29 06:41:59 +0000

    Sustituir boostrap 3.3

--- v1/app.js
+++ v1/app.js
@@ -9,6 +9,12 @@ app.set("view engine", "ejs");
 var campgrounds = [
 		{name: "Sheldon Cooper", image: "https://pixabay.com/get/50e9d4474856b10ff3d8992ccf2934771438dbf852547941752972dd9f4a_340.jpg"},
 		{name: "Leonard Hofstadter", image: "https://pixabay.com/get/52e8d4444255ae14f1dc84609620367d1c3ed9e04e5074417d2f7ddd9e45c2_340.jpg"},
+		{name: "Rajesh Koothrappali ", image: "https://pixabay.com/get/57e8d0424a5bae14f1dc84609620367d1c3ed9e04e5074417d2f7ddd9e45c2_340.jpg"},
+		{name: "Sheldon Cooper", image: "https://pixabay.com/get/50e9d4474856b10ff3d8992ccf2934771438dbf852547941752972dd9f4a_340.jpg"},
+		{name: "Leonard Hofstadter", image: "https://pixabay.com/get/52e8d4444255ae14f1dc84609620367d1c3ed9e04e5074417d2f7ddd9e45c2_340.jpg"},
+		{name: "Rajesh Koothrappali ", image: "https://pixabay.com/get/57e8d0424a5bae14f1dc84609620367d1c3ed9e04e5074417d2f7ddd9e45c2_340.jpg"},
+		{name: "Sheldon Cooper", image: "https://pixabay.com/get/50e9d4474856b10ff3d8992ccf2934771438dbf852547941752972dd9f4a_340.jpg"},
+		{name: "Leonard Hofstadter", image: "https://pixabay.com/get/52e8d4444255ae14f1dc84609620367d1c3ed9e04e5074417d2f7ddd9e45c2_340.jpg"},
 		{name: "Rajesh Koothrappali ", image: "https://pixabay.com/get/57e8d0424a5bae14f1dc84609620367d1c3ed9e04e5074417d2f7ddd9e45c2_340.jpg"}
 	]
 	
